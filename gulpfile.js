53c2d12bca0199fba4b2e57dbdd5bf5359f4fd84
Камиль Садеков <kamil@MacBook-Air-Kamil.local>
2019-12-02 17:54:51 +0300

    include gulp & made gulpfile.js & included browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+gulp.task("hello", function(done){
+    console.log("hello world");
+    done();
+})
+
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
