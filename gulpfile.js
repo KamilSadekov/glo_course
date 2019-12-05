9eb404a43de066f04c399cbb7d083b8e52bf62d8
Камиль Садеков <kamil@MacBook-Air-Kamil.local>
2019-12-05 10:26:15 +0300

    ft: added sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,55 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const minify = require('gulp-minify-css');
-const rename = require('gulp-rename');
-
-gulp.task("hello", function(done){
-    console.log("hello world");
-    done();
-})
+// const minify = require('gulp-minify-css');
+// const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 
-gulp.task('browser-sync', function () {
+ function bs () {
+     serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
 
 
 
-gulp.task('minify', function () {
-gulp.src('./*.css')
-.pipe(minify({keepBreaks: true}))
-.pipe(rename({
-suffix: '.min'
-}))
-.pipe(gulp.dest('./'))
-;
-});
+// gulp.task('minify', function () {
+//     gulp.src('./*.css')
+//         .pipe(minify({
+//             keepBreaks: true
+//         }))
+//         .pipe(rename({
+//             suffix: '.min'
+//         }))
+//         .pipe(gulp.dest('./'));
+// });
 
-gulp.task('default', ['minify'], function() {
+// gulp.task('default', ['minify'], function () {
 
-});
+// });
+
+
+
+
+
+
+
+
+
+
+
+ function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+}; 
+
+
+exports.serve = bs;
