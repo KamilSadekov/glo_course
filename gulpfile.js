551906e25e84f3933f78dfc3fbab78508d7c9d9d
Камиль Садеков <kamil@MacBook-Air-Kamil.local>
2019-12-08 16:16:56 +0300

    ft: included gulp sass etc

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+// Static server
+ function bs() {
+     serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+
+ function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+
+exports.serve = bs;
