55946ae69b5640ee0b10f941a311fc15332ac1ac
Камиль Садеков <kamil@MacBook-Air-Kamil.local>
2019-12-20 10:53:00 +0100

    ft: add swiper and initial this in main.js file

--- js/main.js
+++ js/main.js
@@ -22,3 +22,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
       }
     });
   });
+
+  $(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+  
+    next.css('left', prev.width() + 37 + bullets.width() + 37)
+    bullets.css('left', prev.width() + 37)
+  });
+  
+  
+
+  
