86197e0ac771972d3e3377c67877f9635b2ebff9
Камиль Садеков <kamil@MacBook-Air-Kamil.local>
2020-01-02 13:28:28 +0100

    aded 2 templates

--- js/main.js
+++ js/main.js
@@ -1,169 +1,191 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  };
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-
-  document.body.addEventListener('click', (event) => {
-    let target = event.target;
-    if (target.matches('.modal__close') || target.matches('#popup-window')) {
-      switchModal();
-    }
-  });
-
-  document.addEventListener('keyup', (event) => {
-    if (event.code === 'Escape') {
-      modal.classList.remove('modal--visible');
-    }
-  });
-});
-
 $(document).ready(function () {
-  var mySwiper = new Swiper('.swiper-container', {
+  new WOW().init();
+  /* $(window).scroll(function(){
+      $('.card__image ').addClass('card__animation', $(this).scrollTop() > 3000);
+  }); */
+  var modal = $('.modal'),
+      modalDialog = $('.modal__dialog'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+      modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+      });
+      closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+      });
+      $(document).keyup(function (event) {
+        if(event.which == '27') {
+          modal.toggleClass('modal--visible');
+        };
+      });
+      $(document).on('click', function (event) {
+        if (modal.is(event.target)) {
+          modal.toggleClass('modal--visible');
+        };
+      });
+      $(window).scroll(function () {
+        if($(this).scrollTop() > 1000) {
+          $('.button-up'). fadeIn();
+        } else {
+          $('.button-up').fadeOut();
+        }
+      });
+      $('#button-up').on('click', function (event) {
+        event.preventDefault;
+        $('html, body').animate({scrollTop:0},'300');
+      });
+  var projectSwiper = new Swiper('.projects-swiper-container', {
+    // Optional parameters
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.projects-swiper-pagination',
       type: 'bullets',
+      clickable: true
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.projects-swiper-button-next',
+      prevEl: '.projects-swiper-button-prev',
     },
   });
-  var next = $('.swiper-button-next');
-  var prev = $('.swiper-button-prev');
-  var bullets = $('.swiper-pagination');
+        
+  var next = $('.projects-swiper-button-next');
+  var prev = $('.projects-swiper-button-prev');
+  var bullets = $('.projects-swiper-pagination');
+        
+      next.css('left', prev.width() + bullets.width() + 45)
+      bullets.css('left', prev.width() + 25)
 
-  next.css('left', prev.width() + 37 + bullets.width() + 37)
-  bullets.css('left', prev.width() + 37)
+  var stepsSwiper = new Swiper('.steps__swiper-container', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: {
+      el: '.steps__swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.steps__swiper-button-next',
+      prevEl: '.steps__swiper-button-prev',
+    },
+  });
+        
+  var next2 = $('.steps__swiper-button-next');
+  var prev2 = $('.steps__swiper-button-prev');
+  var bullets2 = $('.steps__swiper-pagination');
 
-  new WOW().init();
+  next2.css('left', prev2.width() + bullets2.width() + 40)
+  bullets2.css('left', prev2.width() + 23)
+        
+    $('.steps__tabs-item').on('click', function () {
+      $('.steps__tabs-item').removeClass('active');
+      $(this).addClass('active');
+      const event = $(this).data('index');
+      stepsSwiper[0].slideTo(event);
+      stepsSwiper[1].slideTo(event);
+    })
+        
+    stepsSwiper[0].on('slideChange', (function () {
+      let event = stepsSwiper[0].activeIndex - 1;
+      if (event === 6) {event=0};
+      $('.steps__tabs-item').removeClass('active');
+      $('.steps__tabs-item').eq(event).addClass('active');
+    }))
 
-  // валидация формы modal__form
-  $('.modal__form').validate({
-    errorClass: "invalid",
-    errorElement: "div",
-    
-    rules: {
-      // строчное правило
-      userName: {
-        required: true,
-        minlength: 2
+  //Валидация формы
+  function validateForm(form){
+    $(form).validate({
+      errorClass: "invalid",
+      rules: {
+        // simple rule, converted to {required:true}
+        policyCheckbox: "required",
+        userName: {
+          required: true,
+          minlength: 2,
+          maxlength: 10
+        },
+        userPhone: "required",
+        userQuetion: {
+          required: true,
+          minlength: 20,
+          maxlength: 400
+        },
+        // compound rule
+        userEmail: {
+          required: true,
+          email: true
+        }
       },
-      userPhone: "required",
-      // правило объект (блок)
-      userEmail: {
-        required: true,
-        email: true
+      errorElement: "div",
+      messages: {
+        userName: {
+          required: "Имя обязательно",
+          minlength: "Имя не короче двух букв"
+        },
+        policyCheckbox: "Подтвердите согланисе на обработку данных",
+        userPhone: "Телефон обязателен",
+        userQuetion: {
+          required: "Вопрос обязателен",
+          minlength: "Вопрос слишком короткий",
+          maxlength: "Слишком длинный вопрос"
+        },
+        userEmail: {
+          required: "Обязательно укажите email",
+          email: "Введите в формате name@domain.com"
+        }
       }
-    },  // сообщения
-    messages: {
-      userName: {
-        required: "Введите ваше имя",
-        minlength: "Имя не короче 2 букв",
-      },
-      userPhone: "Введите ваш номер телефона",
-      userEmail: {
-        required: "Заполните поле",
-        email: "Введите корректный email в формате: name@domain.com"
-      }
-    },
-    submitHandler: function (form) {
-      $.ajax({
-        type: "POST",
-        url: "send.php",
-        data: $(form).serialize(),
-        success: function (response) {
-          alert('Форма отправлена, мы свяжемся с вами, через 10 минут');
-          $(form)[0].reset();
-          modal.classList.remove('modal--visible');
-        }
-      });
-    }
+    });
+  }
+  validateForm('.modal__form');
+  validateForm('.control__form');
+  validateForm('.footer__form');
 
-  });
+  $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
 
+  //Создание карт
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [47.244734, 39.723227],
+            zoom: 18
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
 
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
 
-  // валидация формы control__form
-  $('.control__form').validate({
-    errorClass: "invalid",
-    errorElement: "div",
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш офис',
+            balloonContent: 'второй этаж'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/map-marker.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
 
-    rules: {
-      // строчное правило
-      userName: {
-        required: true,
-        minlength: 2
-      },
-      userPhone: "required"
-    },
-     // сообщения
-    messages: {
-      userName: {
-        required: "Введите ваше имя",
-        minlength: "Имя не короче 2 букв",
-      },
-      userPhone: "Введите ваш номер телефона"
-    },
-    submitHandler: function (form) {
-      $.ajax({
-        type: "POST",
-        url: "send.php",
-        data: $(form).serialize(),
-        success: function (response) {
-          console.log('Ajax сработал. Ответ сервера: ' + response);
-          alert('Форма отправлена, мы свяжемся с вами, через 10 минут');
-          $(form)[0].reset();
-        }
-      });
-    }
-  });
-
-
-  //валидация формы footer__form
-  $('.footer__form').validate({
-    errorClass: "invalid",
-    errorElement: "div",
-
-    rules: {
-      // строчное правило
-      userName: {
-        required: true,
-        minlength: 2
-      },
-      userPhone: "required",
-      userQuestion: "required"
-      // правило объект (блок)
-    },  // сообщения
-    messages: {
-      userName: {
-        required: "Введите ваше имя",
-        minlength: "Имя не короче 2 букв",
-      },
-      userPhone: "Введите ваш номер телефона",
-      userQuestion: "Задайте ваш вопрос"      
-    },
-    submitHandler: function(form) {
-      $.ajax({ 
-        type: "POST",
-        url: "send.php",
-        data: $(form).serialize(),
-        success: function (response) {
-          console.log('Ajax сработал. Ответ сервера: ' + response);
-          alert('Форма отправлена, мы свяжемся с вами, через 10 минут');
-          $(form)[0].reset();
-        }
-      });
-    }
-  });
+    myMap.geoObjects
+        .add(myPlacemark)
+        myMap.behaviors
+        // Отключаем часть включенных по умолчанию поведений:
+        //  - drag - перемещение карты при нажатой левой кнопки мыши;
+        //  - magnifier.rightButton - увеличение области, выделенной правой кнопкой мыши.
+        .disable(['scrollZoom', 'rightMouseButtonMagnifier'])
+        // Включаем линейку.
+        .enable('ruler');
 });
-  // маска для телефона
-  $(document).ready(function () {
-    $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
-  });
+
+});
--- send.php
+++ send.php
@@ -1,44 +1,33 @@
 <?php
-
 $userName = $_POST['userName'];
 $userEmail = $_POST['userEmail'];
 $userPhone = $_POST['userPhone'];
-
-
 // Load Composer's autoloader
-require 'phpmailer/Exception.php';
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
-
+require 'phpmailer/Exception.php';
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer\PHPMailer\PHPMailer();
-
 try {
     //Server settings
     $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp1.gmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'kamsadead@gmail.com';                     // SMTP username
     $mail->Password   = 'gomge1-myxzoz-qYfvyn';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
-
     //Recipients
-    $mail->setFrom('kamsadead@gmail.com', 'Kamil');
-    $mail->addAddress('sadekovk98@mail.ru');     // Add a recipient
-
+    $mail->setFrom('kamsadead@gmail.com', 'Ремонт квартир');
+    $mail->addAddress('kamiltrofe111163@gmail.com', 'Камиль');     // Add a recipient
+  
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Новая заявка сайта';
-    $mail->Body    = "имя пользователя:${userName}, его телефон:${userPhone}, и его почта:${userEmail}";
-
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
     $mail->send();
     header('Location: thanks.html');
 } catch (Exception $e) {
-    echo "Ошибка отправки. Код ошибки: {$mail->ErrorInfo}";
+    echo "Письмо не отправленно, есть ошибка. Код ошибкт: {$mail->ErrorInfo}";
 }
-
-
-
-?>
